38726af8132254dc7d281786f3fa505f1768d608
jocoso <joshua.collado022@gmail.com>
2026-01-13 19:13:41 -0500

    Added a map that can be rendered as a 2d array.

--- main.py
+++ main.py
@@ -10,23 +10,28 @@ bg_color = (255, 255, 255)
 color_red = (255, 0, 0)
 color_green = (0, 255, 0)
 scr_title = "Cycle of Power"
-scr_iconsrc = 'icc.png'
+scr_iconsrc = "icc.png"
 running = True
+font = pygame
+
 
 def initialize_pygame():
     # Initialize Pygame
     # pygame modules
     (numpass, numfail) = pygame.init()
 
-    print('Number of modules initialized successfully:', numpass)
-    
+    print("Number of modules initialized successfully:", numpass)
+
+
 def is_initialized():
     is_initialized = pygame.get_init()
-    print('Is pygame modules initialized:', is_initialized)
+    print("Is pygame modules initialized:", is_initialized)
     return is_initialized
+
 
 def name_screen(name):
     pygame.display.set_caption(name)
+
 
 def set_screen():
     # Set up the game window
@@ -35,32 +40,36 @@ def set_screen():
     set_icon(scr_iconsrc)
     return screen
 
+
 def load_img(img_src):
     return pygame.image.load(img_src)
+
 
 def set_icon(icon_src):
     icon_img = load_img(icon_src)
     pygame.display.set_icon(icon_img)
 
+
 def paint_screen(screen, color):
     screen.fill(color)
     pygame.display.flip()
 
+
 def main():
     global running
     global bg_color
-    
+
     screen = set_screen()
 
     # ProtoSlide and ClickInput Implementation
     sld_proto = ProtoSlide()
     inp_lftclick = LeftClickInput()
-    
+
     sld_proto.present_slide_name()
-    
+
     if not is_initialized():
         initialize_pygame()
-    
+
     # Game loop
     while running:
         for event in pygame.event.get():
@@ -71,14 +80,14 @@ def main():
 
         paint_screen(screen, bg_color)
 
-        if(bg_color == color_red):
+        if bg_color == color_red:
             bg_color = color_green
         else:
             bg_color = color_red
 
     # Quit Pygame
     pygame.quit()
-    
+
 
 if __name__ == "__main__":
     main()
--- proto.py
+++ proto.py
@@ -3,52 +3,86 @@
 import pygame
 from pygame.locals import *
 from pygame import mouse
+import numpy as np
+
 
 """
     Used to update user inputs.
 """
+
+map = """* * * * * * * *
+*             *
+*             *
+*             *
+***************"""
+
+
 class Input:
     def __init__(self):
         self.name = "__name__"
+
     def change_name(self, in_name):
         self.name = in_name
+
     def update(self, event):
         pass
+
 
 """
     A scene in the game.
 """
+
+
 class Slide:
-    def __init__(self):
-        self.name = "slide_name"
-        self.size = (100, 100)
+    def __init__(self, sld_name):
+        self.name = sld_name
+        self.render()
+
     def change_name(self, sld_name):
         self.name = sld_name
+
     def change_size(self, sld_size):
         if type(sld_size) == tuple:
             self.size = sld_size
+
     def present_slide_name(self):
         print(self.name)
+
+    def render(self):
+        # rows = map.split("\n")
+        # split_rows = [row.split() for row in rows]
+
+        # max_len = max(len(r) for r in split_rows)
+        # padded_rows = [r + [" "] * (max_len - len(r)) for r in split_rows]
+
+        # array = np.array(padded_rows, dtype=str)
+        # print(array)
+        rows = map.split("\n")
+        array = np.array([list(row) for row in rows], dtype=str)
+        print(array)
+
 
 """
     An left click input
 """
+
+
 class LeftClickInput(Input):
     def __init__(self):
         Input.__init__(self)
-        
+
         self.click_code = 1
         self.input = (0, 0)
         self.prev_input = (0, 0)
-        
+
         self.change_name("__click__")
-        
+
     # Return true if the input has changed since the last time you asked.
     def has_input_changed(self):
-    	input_changed = self.input != self.prev_input
-    	self.prev_input = self.input 
-    	return input_changed
-    	
+        input_changed = self.input != self.prev_input
+        self.prev_input = self.input
+        return input_changed
+
     # If the left button was clicked
     # make last input variable hold the new input
     # and replace the input variable with the current click
@@ -58,9 +92,7 @@ class LeftClickInput(Input):
             self.input = mouse.get_pos()
             print(self.input)
 
+
 class ProtoSlide(Slide):
     def __init__(self):
-        Slide.__init__(self)
-        self.change_name("Prototype Scene")
-        self.change_size((200, 200))
-        
+        Slide.__init__(self, "Prototype Scene")
